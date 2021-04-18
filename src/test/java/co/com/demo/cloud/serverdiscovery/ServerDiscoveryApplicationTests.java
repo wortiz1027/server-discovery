6b20c8386a431b10592e739e034c4f2119408c12
Wilman Ortiz <willman.ortiz@gmail.com>
2021-04-18 13:52:50 -0500

    feat: Define proyecto de servidor de descubrimiento

---
+++ src/test/java/co/com/demo/cloud/serverdiscovery/ServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.demo.cloud.serverdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
